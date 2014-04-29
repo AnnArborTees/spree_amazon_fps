99324c167b97b0b7cc9c5198d55570fc307b0b3e
Resonious <metreckk@gmail.com>
2014-04-29 10:51:34 -0400

    Move api call api into the amazon_fps gateway to make it look less like coupling

--- app/models/spree/gateway/amazon_fps.rb
+++ app/models/spree/gateway/amazon_fps.rb
@@ -1,90 +1,130 @@
 module Spree
-	class Gateway::AmazonFps < Gateway
-		preference :access_key, :string
-		preference :secret_key, :string
-		preference :server, :string, default: 'sandbox'
-		preference :logourl, :string, default: ''
+  class Gateway::AmazonFps < Gateway
+    preference :access_key, :string
+    preference :secret_key, :string
+    preference :server, :string, default: 'sandbox'
+    preference :logourl, :string, default: ''
 
-		def supports(source)
-			true
-		end
+    # Begin inner classes for API calls
+    class ApiResponse
+      def initialize(action, response_xml)
+        @action = action
+        @doc = Nokogiri::XML(response_xml)
+      end
 
-		def auto_capture?
-			true
-		end
+      def valid?
+        !@doc.css("#{@action}Response").empty?
+      end
 
-		def method_type
-			'amazon_fps'
-		end
+      def respond_to?(name)
+        !@doc.css("#{@action}Response > #{@action}Result > #{name}").empty?
+      end
 
-		def provider_class
-			return ::Spree::Gateway::AmazonFps
-		end
+      def method_missing(name, *args, &block)
+        super unless respond_to? name
+        @doc.css("#{@action}Response > #{@action}Result > #{name}").first.content
+      end
+    end
 
-		def is_sandbox?
-			preferred_server.present? && preferred_server == 'sandbox'
-		end
+    class ApiCall
+      def initialize(amazon_gateway)
+        @amazon = amazon_gateway
+      end
 
-		def get(pref, default=nil)
-			val = self.send(('preferred_'+pref.to_s).to_sym)
-			if val.present?
-				return val
-			else
-				# TODO use a better error for this
-				raise ArgumentError unless default
-				return default
-			end
-		end
+      def respond_to?(name)
+        call(name, params).valid?
+      end
 
-		def sign_params(parameters, verb)
-			::Amazon::FPS::SignatureUtils.sign_parameters({
-					parameters: parameters,
-					aws_secret_key: get('secret_key'),
-					host: end_point_uri.host,
-					verb: verb,
-					uri: end_point_uri.path,
-					algorithm: parameters[:signatureMethod] || parameters[:SignatureMethod]
-				})
-		end
+      def call(action, params)
+        ApiResponse.new(action, Net::HTTP.get @amazon.api_call_uri(params))
+      end
 
-		def end_point_url_str
-			"https://authorize.#{is_sandbox? ? 'payments-sandbox' : 'payments'}.amazon.com/pba/paypipeline"
-		end
+      def method_missing(name, *args, &block)
+        params = { :Action => name }
+        args.each do |v|
+          params.merge! v if v.kind_of? Hash
+        end
+        resp = call(name, params)
+        super unless resp.valid?
+        return resp
+      end
+    end
+    # End inner classes
 
-		def end_point_uri
-			URI.parse end_point_url_str
-		end
+    def supports(source)
+      true
+    end
 
-		def api_call_uri(options)
-			# This MIGHT be worth moving over to api_call.rb.
+    def auto_capture?
+      true
+    end
 
-			options[:AWSAccessKeyId] = get('access_key') unless options[:AWSAccessKeyId]
-			options[:SignatureVersion] = 2
-			options[:SignatureMethod] = 'HmacSHA256'
-			options[:Timestamp] = Time.now.utc.iso8601
-			options[:Version] = '2008-09-17'
-			options[:Signature] = sign_params(options, get('secret_key'), 'GET') unless options[:Signature]
-			
-			uri = URI("https://fps#{is_sandbox? ? '.sandbox' : ''}.amazonaws.com/")
-			uri.query = options.to_query
-			uri
-		end
+    def method_type
+      'amazon_fps'
+    end
 
-		def api
-			::Amazon::FPS::ApiCall.new(self)
-		end
+    def provider_class
+      return ::Spree::Gateway::AmazonFps
+    end
 
-		def purchase(amount, checkout, options)
-			# Consider verifying the signature here instead of in the Amazon#complete
-			# action.
+    def is_sandbox?
+      preferred_server.present? && preferred_server == 'sandbox'
+    end
 
-			# Also consider running the GetTransactionStatus api call here.
+    def get(pref, default=nil)
+      val = self.send(('preferred_'+pref.to_s).to_sym)
+      if val.present?
+        return val
+      else
+        # TODO use a better error for this
+        raise ArgumentError unless default
+        return default
+      end
+    end
 
-			# ALSO learn how to use Nokogiri for XML or something, then you can
-			# maybe parse the XML contents into a hash or object of some sort.
+    def sign_params(parameters, verb)
+      ::Amazon::FPS::SignatureUtils.sign_parameters({
+          parameters: parameters,
+          aws_secret_key: get('secret_key'),
+          host: end_point_uri.host,
+          verb: verb,
+          uri: end_point_uri.path,
+          algorithm: parameters[:signatureMethod] || parameters[:SignatureMethod]
+        })
+    end
 
-			# Remember this function returns a class that has a success? function
-			# and an authorization function
-		end
-	end
+    def end_point_url_str
+      "https://authorize.#{is_sandbox? ? 'payments-sandbox' : 'payments'}.amazon.com/pba/paypipeline"
+    end
+
+    def end_point_uri
+      URI.parse end_point_url_str
+    end
+
+    def api_call_uri(options)
+      options[:AWSAccessKeyId] = get('access_key') unless options[:AWSAccessKeyId]
+      options[:SignatureVersion] = 2
+      options[:SignatureMethod] = 'HmacSHA256'
+      options[:Timestamp] = Time.now.utc.iso8601
+      options[:Version] = '2008-09-17'
+      options[:Signature] = sign_params(options, 'GET') unless options[:Signature]
+      
+      uri = URI("https://fps#{is_sandbox? ? '.sandbox' : ''}.amazonaws.com/")
+      uri.query = options.to_query
+      uri
+    end
+
+    def api
+      ApiCall.new(self)
+    end
+
+    def purchase(amount, checkout, options)
+      # Also consider running the GetTransactionStatus api call here.
+
+      # Remember this function returns a class that has a success? function
+      # and an authorization function 
+
+
+    end
+  end
 end
--- lib/spree_amazon_fps.rb
+++ lib/spree_amazon_fps.rb
@@ -1,4 +1,3 @@
 require 'spree_core'
 require 'spree_amazon_fps/engine'
 require 'amazon/fps/signature_utils'
-require 'amazon/fps/api_call'
