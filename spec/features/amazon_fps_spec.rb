a16df26ccbac94075c154e4938cc2d7000963905
Resonious <metreckk@gmail.com>
2014-05-06 14:13:46 -0400

    Progress on amazon fps feature spec(s)

--- spec/features/amazon_fps_spec.rb
+++ spec/features/amazon_fps_spec.rb
@@ -4,8 +4,24 @@ describe 'As a user', js: true do
   let!(:user) {create(:user)}
 
   context 'with a valid order' do
-    let!(:order) {create(:order_with_line_items)}
+    let!(:country) { create(:country, :states_required => true) }
+    let!(:state) { create(:state, :country => country) }
+    let!(:shipping_method) { create(:shipping_method) }
+    let!(:stock_location) { create(:stock_location) }
+    let!(:mug) { create(:product, :name => "RoR Mug") }
+    let!(:payment_method) { create(:check_payment_method) }
+    let!(:zone) { create(:zone) }
+
+    let!(:user) {create(:user_with_addreses)}
     let!(:product) {create(:product, name: "Test Product")}
+    let!(:amazon_payment) {create(:amazon_payment)}
+
+    before :each do
+      amazon_payment.stub preferred_access_key: 'AcCeSs0BCA321'
+      amazon_payment.stub preferred_secret_key: 'SeCrEt0ABC123'
+      Spree::CheckoutController.any_instance.stub try_spree_current_user: user
+      Spree::CheckoutController.any_instance.stub skip_state_validation?: true
+    end
 
     it 'I can pay using Amazon FPS' do
       visit '/'
@@ -15,12 +31,15 @@ describe 'As a user', js: true do
         fill_in 'First Name', with: 'Test'
         fill_in 'Last Name', with: 'Johnson'
         fill_in 'Street Address', with: '123 test st.'
-        fill_in 'City', with: 'testcity'
-        select 'Alabama', from: 'order_bill_address_attributes_state_id'
-        fill_in 'Zip', with: '48103'
+        fill_in 'City', with: 'Acmar'
+        find('#order_bill_address_attributes_state_id').find(:xpath, 'option[2]').select_option
+        fill_in 'Zip', with: '35004'
         fill_in 'Phone', with: '734-123-4567'
       end
       click_button 'Save and Continue'
+      click_button 'Save and Continue'
+      choose 'Amazon'
+      find('#amazon_button').click
     end
   end
 
