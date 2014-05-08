3af5e66b33918ade574cad92c9d68cf184db0393
Resonious <metreckk@gmail.com>
2014-05-08 13:34:08 -0400

    Converted some tabs to spaces

--- spec/support/amazon_fps_helpers.rb
+++ spec/support/amazon_fps_helpers.rb
@@ -1,26 +1,26 @@
 module AmazonFpsHelpers
-	def amazon_access_key
-		grab('access_key') or 'AKIAJRZRFCCMMSLTZSEQ'
-	end
-	def amazon_secret_key
-		grab('secret_key') or '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'
-	end
-	def amazon_email
-		grab('email') or 'spree-amazon-fps@mailinator.com'
-	end
-	def amazon_password
-		grab('password') or '6dY4haWB'
-	end
+  def amazon_access_key
+    grab('access_key') or 'AKIAJRZRFCCMMSLTZSEQ'
+  end
+  def amazon_secret_key
+    grab('secret_key') or '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'
+  end
+  def amazon_email
+    grab('email') or 'spree-amazon-fps@mailinator.com'
+  end
+  def amazon_password
+    grab('password') or '6dY4haWB'
+  end
 
-	def amazon_fps_url
+  def amazon_fps_url
     'https://payments-sandbox.amazon.com/sdui/sdui/overview'
   end
 
 private
-	def grab(name)
-		filename = "#{Rails.root}/config/amazon_fps_tests.yml"
-		return unless File.exists? filename
-		YAML.load_file(filename)[name]
-	end
+  def grab(name)
+    filename = "#{Rails.root}/config/amazon_fps_tests.yml"
+    return unless File.exists? filename
+    YAML.load_file(filename)[name]
+  end
 
 end
